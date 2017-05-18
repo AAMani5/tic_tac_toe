f747f58f25d7c01be5b3478d16ac076bd8da8af4
Ashwini <ashie.oh@gmail.com>
2017-05-18 18:58:39 +0100

    Test drive creation of claim_field method

--- lib/board.rb
+++ lib/board.rb
@@ -1,17 +1,25 @@
 class Board
+  attr_reader :get_table
+  TABLE = [:zero, :one, :two, :three, :four, :five, :six, :seven, :eight]
 
   def initialize
-    create_table
+    @get_table = TABLE
   end
 
-  def get_table
-    @table
+  def claim_field(position, player_symbol)
+    fail "outofbound" if field_location_outofbound(position)
+    fail "occupied" if field_occupied(position)
+    @get_table = @get_table.map{|pos| pos == position ? player_symbol : pos}
   end
 
   private
 
-  def create_table
-    @table = [:zero, :one, :two, :three, :four, :five, :six, :seven, :eight]
+  def field_location_outofbound(position)
+    !(TABLE.include? position)
+  end
+
+  def field_occupied(position)
+    !(get_table.include? position)
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,4 +8,20 @@ describe Board do
     expect(board.get_table).to eql table
   end
 
+  context "#claim_field" do
+    it "claim a non claimed field" do
+      expect(board.claim_field(:two, :x)).to eql [:zero, :one, :x, :three, :four, :five, :six, :seven, :eight]
+    end
+
+    it "raise exception if field already claimed" do
+      board.claim_field(:two, :x)
+      expect{board.claim_field(:two, :x)}.to raise_error "occupied"
+    end
+
+    it "raise exception if field position out of bound" do
+      expect{board.claim_field(:nine, :x)}.to raise_error "outofbound"
+    end
+
+  end
+
 end
