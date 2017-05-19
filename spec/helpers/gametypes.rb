f2669d2c3c4d330a1847c9c82cf104d85b51221f
Ashwini <ashie.oh@gmail.com>
2017-05-19 11:52:50 +0100

    Add helper methods for different game state

--- spec/helpers/gametypes.rb
+++ spec/helpers/gametypes.rb
@@ -16,4 +16,28 @@ module GameTypes
     board.claim_field(:four, :x)
   end
 
+  def play_row_claimed_game
+    board.claim_field(:zero, :x)
+    board.claim_field(:three, :o)
+    board.claim_field(:one, :x)
+    board.claim_field(:six, :o)
+    board.claim_field(:two, :x)
+  end
+
+  def play_column_claimed_game
+    board.claim_field(:zero, :x)
+    board.claim_field(:one, :o)
+    board.claim_field(:three, :x)
+    board.claim_field(:two, :o)
+    board.claim_field(:six, :x)
+  end
+
+  def play_diagonal_claimed_game
+    board.claim_field(:zero, :x)
+    board.claim_field(:one, :o)
+    board.claim_field(:four, :x)
+    board.claim_field(:two, :o)
+    board.claim_field(:eight, :x)
+  end
+
 end
