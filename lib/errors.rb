be334bab29b57df056df26f4d913a4c6a97587c3
Ashwini <ashie.oh@gmail.com>
2017-05-21 10:10:02 +0100

    Add messages to custom exception class

--- lib/errors.rb
+++ lib/errors.rb
@@ -1,6 +1,26 @@
 module Errors
-  class OutOfBound < StandardError; end
-  class Occupied < StandardError; end
-  class NotPlayersTurn < StandardError; end
-  class GameOver < StandardError; end
+  class OutOfBound < StandardError
+    def message
+      "Error Message: Field outside available area"
+    end
+  end
+
+  class Occupied < StandardError
+    def message
+      "Error Message: Field already occupied"
+    end
+  end
+
+  class NotPlayersTurn < StandardError
+    def message
+      "Error Message: Wrong player, It is not your turn"
+    end
+  end
+
+  class GameOver < StandardError
+    def message
+      "Error Message: Game over, Invalid move"
+    end
+  end
+
 end
