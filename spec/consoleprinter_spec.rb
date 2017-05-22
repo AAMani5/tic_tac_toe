0c9754f3f82cb2edfde201f5d461d0e86ad40c70
Ashwini <ashie.oh@gmail.com>
2017-05-22 09:18:05 +0100

    Test methods to print table and confirm option choosen

--- spec/consoleprinter_spec.rb
+++ spec/consoleprinter_spec.rb
@@ -3,10 +3,21 @@ require 'consoleprinter'
 describe ConsolePrinter do
   let(:table) {[:zero, :one, :two]}
   let(:printer) {ConsolePrinter.new}
-  let(:console) {StringIO.new}
 
   it "prints a welcome message and board" do
     expect{ printer.welcome_message(table) }.to output("Welcome to Tic Tac Toe - Fun interactive game\nzero\t|\tone\t|\ttwo\n").to_stdout
+  end
+
+  it "displays game options" do
+    expect { printer.display_gametype_options }.to output("You can choose from the following options to begin\n0. Human vs. Human\n1. Human vs. Computer\n2. Computer vs. Computer\n").to_stdout
+  end
+
+  it "confirms option choosen" do
+    expect { printer.option_chosen("0") }.to output("You have chosen option 0\n").to_stdout
+  end
+
+  it "print the table" do
+    expect { printer.print_board(table) }.to output("zero\t|\tone\t|\ttwo\n").to_stdout
   end
 
 end
