9928275a369da9c433059d1d84a226b2f09bb81c
Ashwini <ashie.oh@gmail.com>
2017-05-22 15:18:30 +0100

    Test the return value when computer makes the first field claim

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -1,8 +1,35 @@
 require_relative 'player'
+require_relative 'board'
 
 class ComputerPlayer
   include Player
 
-  def make_move
+  def make_move(board, opponent_sym)
+    return :one if first_move?(board.get_table)
   end
+
+  private
+
+  def first_move?(table)
+    Board::TABLE & table == Board::TABLE
+  end
+
+  def assign_score(board, opponent_sym)
+    return 1 if i_am_winning?
+    return -1 if opponent_is_winning?
+    return 0 if tie_game?
+  end
+
+  def i_am_winning?
+    board.winning_fields_claimed_by(symbol, opponent_sym) == symbol
+  end
+
+  def opponent_is_winning?
+    board.winning_fields_claimed_by(symbol, opponent_sym) == opponent_sym
+  end
+
+  def tie_game?
+    board.all_fields_claimed?(symbol, opponent_sym) && !board.winning_fields_claimed?(symbol, opponent_sym)
+  end
+
 end
--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -1,14 +1,25 @@
 require 'computer_player'
+require 'board'
 
 describe ComputerPlayer do
   let(:computer_player) {ComputerPlayer.new(:o)}
+  let(:board) {Board.new}
 
   it "#symbol" do
     expect(computer_player.symbol).to eql :o
   end
 
-  it "#" do
-    expect{computer_player.make_move}.to_not raise_error
+  context "#make_move" do
+
+    it "makes its first move" do
+      expect(computer_player.make_move(board, :o)).to eql :one
+    end
+
+    it "" do
+      board.claim_field(:zero, :o)
+      expect(computer_player.make_move(board, :o)).to eql nil
+    end
+
   end
 
 end
