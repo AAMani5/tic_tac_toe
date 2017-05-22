b53b1ed7a7815cbf01fbd8c0f3c80cff6bc9cb11
Ashwini <ashie.oh@gmail.com>
2017-05-22 15:48:28 +0100

    Add current state of the game as parameter to computer player's move calculation

--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -1,23 +1,27 @@
 require 'computer_player'
 require 'board'
+require 'human_player'
+require 'game'
 
 describe ComputerPlayer do
-  let(:computer_player) {ComputerPlayer.new(:o)}
+  let(:computer_player) {ComputerPlayer.new(:x)}
+  let(:human_player) {HumanPlayer.new(:o)}
   let(:board) {Board.new}
+  let(:game) {Game.new(board, human_player, computer_player)}
 
   it "#symbol" do
-    expect(computer_player.symbol).to eql :o
+    expect(computer_player.symbol).to eql :x
   end
 
   context "#make_move" do
 
     it "makes its first move" do
-      expect(computer_player.make_move(board, :o)).to eql :one
+      expect(computer_player.make_move(game, board, :o)).to eql :one
     end
 
-    it "" do
-      board.claim_field(:zero, :o)
-      expect(computer_player.make_move(board, :o)).to eql nil
+    it "doesnot return a field without calculating the best move" do
+      game.make_mark(:zero, human_player)
+      expect(computer_player.make_move(game, board, :o)).to eql nil
     end
 
   end
